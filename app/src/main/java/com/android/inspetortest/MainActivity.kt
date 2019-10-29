ecda4dfbcbf10693c189b409de64edc1d987ac14
Lourenco Biselli <lourenco@useinspetor.com>
2019-10-29 09:07:54 -0300

    Small bug fix

--- app/src/main/java/com/android/inspetortest/MainActivity.kt
+++ app/src/main/java/com/android/inspetortest/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        Inspetor.sharedInstance().setup("inspetor.test", "123", true, false)
+        Inspetor.sharedInstance().setup("inspetor.demo", "58657", true, false)
 
         val intent = Intent(this, SecondActivity::class.java)
         startActivity(intent)
